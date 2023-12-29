862d05888746492359796312bc971616611193c6
Sujal Gupta <sujalgvs987@gmail.com>
2023-12-30 00:27:24 +0530

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using UserLoginAuthentication.Areas.Identity.Data;
+using UserLoginAuthentication.Models;
+
+namespace UserLoginAuthentication.Controllers
+{
+    [Authorize]
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly UserManager<AppUser> _userManager;
+
+        public HomeController(ILogger<HomeController> logger, UserManager<AppUser> userManager)
+        {
+            _logger = logger;
+            this._userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            ViewData["UserID"] = _userManager.GetUserId(this.User);
+            ViewData["UserName"] = _userManager.GetUserName(this.User);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UserLoginAuthentication.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
